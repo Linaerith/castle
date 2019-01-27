34888a0cd66c3377154d3ef013f326381a3e6e9b
Lindsay Grondin <grondin.lindsay@gmail.com>
2019-01-27 14:00:27 +0100

    Script from RelaisChateau obtained

--- public/scraping.js
+++ public/scraping.js
@@ -1,17 +1,13 @@
-const rp = require('request-promise');
-const $ = require('cheerio');
-const url = 'https://www.relaischateaux.com/us/destinations/europe/france';
+const request = require('request');
+const cheerio = require('cheerio');
 
+request('https://www.relaischateaux.com/us/destinations/europe/france', (error, response, html)=> {
+  if(!error && response.statusCode == 200) {
+    const $ = cheerio.load(html);
+    const siteResult = $ ('.overmapWrap');
 
-rp(url)
-  .then(function(html){
-    //success!
-    const wikiUrls = [];
-    for (let i = 0; i < 150; i++) {
-      wikiUrls.push($('#destinationResults > div:nth-child(3) > div > div:nth-child(1) > div > div > div > div.slick-slide.slick-active > div > span', html)[i].attribs.href);
-    }
-    console.log(wikiUrls);
-  })
-  .catch(function(err){
-    //handle error
-  });
+    //console.log(siteResult.html());
+    const output = siteResult.find('script').html();
+    console.log(output.markers);
+  }
+})
