76fe9303f370713aefe198421d7eccba993ef429
Lindsay Grondin <grondin.lindsay@gmail.com>
2019-02-07 00:01:39 +0100

    scrapping done for the hotels but a bug remain in the listing, and scraping almost done for the restaurants

--- public/scraping.js
+++ public/scraping.js
@@ -3,45 +3,53 @@ const cheerio = require('cheerio');
 const fs = require('fs');
 const writeStream = fs.createWriteStream('output.js');
 
-request('https://www.relaischateaux.com/us/destinations/europe/france', (error, response, html)=> {
-  if(!error && response.statusCode == 200) {
-    const $ = cheerio.load(html);
-    const siteResult = $ ('.overmapWrap');
+/*
+request('https://www.relaischateaux.com/us/site-map/etablissements', function (error, response, html) {
+  if (!error && response.statusCode == 200) {
+    var $ = cheerio.load(html);
+    const hotels = $('#countryF');
+    //const france = hotels.next().html();
+    //console.log(france);
+    const list = hotels.next().children().next();
+    //console.log(list.html());
+    list.children().children().each(function(i, element){
+      var a = $(this);
+      var title = a.text();
+      var url = a.attr('href');
+      var str = title.replace(/\s\s+/g, " ")
 
-    //console.log(siteResult.html());
-    const output = siteResult.find('script').html();
-    //console.log(output);
+      // Our parsed meta data object
+      var metadata = {
+        title: str,
+        url: url
+      };
+      //console.log(a.html());
+      console.log(metadata);
+    });
   }
-})
-//https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin/https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin/hotel-restaurant
-request('https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin', (error, response, html)=> {
-  if(!error && response.statusCode == 200) {
-    const $ = cheerio.load(html);
-    const siteResult = $ ('.poi-search-result');
+});*/
 
-    //console.log(siteResult.html());
-    const output = siteResult.text();
-    console.log(output);
-  }
-})
+request('https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin', function (error, response, html) {
+  if (!error && response.statusCode == 200) {
+    var $ = cheerio.load(html);
+    const hotels = $('.poi-search-result');
+    //const france = hotels.next().html();
+    //console.log(france);
+    const list = hotels.find('block__content');
+    //console.log(hotels.html());
+    hotels.children().children().each(function(i, element){
+      var a = $(this).children();
+      var title = a.parent().attr('attr-gtm-title');
+      var url = 'https://restaurant.michelin.fr' + a.attr('href');
+      //var str = title.replace(/\s\s+/g, " ")
 
-/*function Payment()
-{
-  for(const act of actors){
-    let evt = getEvent(act.eventId);
-    for(const pay of act.payment){
-      if(pay.who === 'booker'){
-        pay.amount = evt.price;
-      }
-      if(pay.who === 'insurance'){
-        pay.amount = evt.commission.insurance;
-      }
-      if(pay.who === 'treasury'){
-        pay.amount = evt.commission.treasury;
-      }
-      if(pay.who === 'privateaser'){
-        pay.amount = evt.commission.privateaser;
-      }
-    }
+      // Our parsed meta data object
+      var metadata = {
+        title: title,
+        url: url
+      };
+      console.log(metadata);
+      //console.log(a.html());
+    });
   }
-};*/
+});
