519974185d93dd67287a8bf67e4dcd23b0020fb8
Lindsay Grondin <grondin.lindsay@gmail.com>
2019-02-07 08:47:35 +0100

    scrapping done for the restaurants

--- public/scraping.js
+++ public/scraping.js
@@ -53,3 +53,34 @@ request('https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-
     });
   }
 });
+
+
+var step;
+for(step = 2; step < 36; step ++)
+{
+  var link = 'https://restaurant.michelin.fr/restaurants/france/restaurants-1-etoile-michelin/restaurants-2-etoiles-michelin/restaurants-3-etoiles-michelin/page-' + step;
+  request(link, function (error, response, html) {
+    if (!error && response.statusCode == 200) {
+      var $ = cheerio.load(html);
+      const hotels = $('.poi-search-result');
+      //const france = hotels.next().html();
+      //console.log(france);
+      const list = hotels.find('block__content');
+      //console.log(hotels.html());
+      hotels.children().children().each(function(i, element){
+        var a = $(this).children();
+        var title = a.parent().attr('attr-gtm-title');
+        var url = 'https://restaurant.michelin.fr' + a.attr('href');
+        //var str = title.replace(/\s\s+/g, " ")
+
+        // Our parsed meta data object
+        var metadata = {
+          title: title,
+          url: url
+        };
+        console.log(metadata);
+        //console.log(a.html());
+      });
+    }
+  });
+}
