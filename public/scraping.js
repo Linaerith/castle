9cc20d7900d738d10925c3f63975830c2897d863
Lindsay Grondin <grondin.lindsay@gmail.com>
2019-02-17 16:31:57 +0100

    scraping done

--- public/scraping.js
+++ public/scraping.js
@@ -2,157 +2,60 @@ const request = require('request');
 const cheerio = require('cheerio');
 const fs = require('fs');
 const writeStream = fs.createWriteStream('relais.csv');
-writeStream.write(`Hotel; Price; Restaurants; Url\n`);
+writeStream.write(`Hotel; Chef; CodePostal; Url\n`);
 
-(async function() {
-  try{
-const main = await request('https://www.relaischateaux.com/us/site-map/etablissements', function (error, response, html) {
+
+request('https://www.relaischateaux.com/us/site-map/etablissements', function (error, response, html) {
   if (!error && response.statusCode == 200) {
     var $ = cheerio.load(html);
     const hotels = $('#countryF');
-    //const france = hotels.next().html();
-    //console.log(france);
+
     const list = hotels.next().first().children().next();
-    //console.log(list.html());
-    list.children().children().each(function(i, element){
-      var a = $(this);
-      var title = a.text();
-      var url = a.attr('href');
+
+    list.children().each(function(i, element){
+      const a = $(this);
+
+      //Take the hotel's name and url
+      var title = a.find('a').first().text();
+      var url = a.find('a').first().attr('href');
       var str = title.replace(/\s\s+/g, " ")
-      console.log(a.next().first().html());
 
+      //Take the chief url
+      if(a.find('a').next().first().length > 0 && a.find('a').next().next().first().length > 0){
+      var chiefurl = a.find('a').next().first().attr('href');
 
-/* METTRE NOM DE CHEF ICI
-      var chiefname = a.next().first().text();
-      console.log(chiefname);*/
-
-      // Our parsed meta data object
-      var metadata = {
-        title: str,
-        url: url
-      };
-      //console.log(a.html());
-    //  console.log(metadata);
-
-/* SCRAPING THE HOTEL PAGES*/
-    //SECOND REQUEST
-    (async function() {
-      try{
-    const main = await request(url, function (error, response, html) {
-        if (!error && response.statusCode == 200) {
-          var s = cheerio.load(html);
-          const rest = s('.jsSecondNavMain');
-          const list = rest.children().next().find('a').first();
-          var urlRestaurant = list.attr('href');
-          //console.log(urlRestaurant);
-
-          const findPrice = s('.priceTag');
-          var price = findPrice.children().children().first().text();
-
-        //  var price = findPrice.find('.price').first().text();
-          console.log(price);
-
-          if(typeof urlRestaurant !== 'undefined' && urlRestaurant.match("restaurant")){
-
-            // THIRD REQUEST
-            (async function() {
-              try{
-            const main = await request(urlRestaurant, function (error, response, html) {
+      //Request to take the chief name and code postal
+        request(chiefurl, function (error, response, html) {
               if (!error && response.statusCode == 200) {
-                var c = cheerio.load(html);
-                const restos = c('.jsSecondNavSub');
-                if(restos.length >0){
-                  const listrestos = restos.children();
-                  //console.log(restos.html());
-                  listrestos.each(function(i, element){
-                    var l = c(this);
-                    //console.log(l.html());
+                const z = cheerio.load(html);
 
-                    var nomRestaurant = l.find('a').first();
-                    var name = nomRestaurant.text();
-                    //console.log(nomRestaurant);
-                    var string = name.replace(/\s\s+/g, " ");
-                    //console.log(string);
-/* FOURTH REQUEST*/
-                    if(string.match("Other restaurants")){
+                //Take the chief name
+                const findChiefName = z('.row');
+                var chiefName = findChiefName.find('h1').text();
+                //console.log(chiefName);
 
-                      var urlother = nomRestaurant.attr('href');
-                      //console.log(urlother);
-                      (async function() {
-                        try{
-                      const main = await  request(urlother, function (error, response, html) {
-                        if (!error && response.statusCode == 200) {
-                          var o = cheerio.load(html);
-                          const restos = o('.hotelTabsHeaderTitle');
-                          //mainTitle2.noVerticalMargin.other-restaurant-title
-                          restos.each(function(i, element){
-                            var li = o(this);
-                            //console.log(li.html());
+                //Take the chief postal code
+                const findPostalCode = z("*[itemprop = 'address']");
+                var postalCode = findPostalCode.find('[itemprop=postalCode]').text();
+                var postalCodeSTR = postalCode.replace(/\s\s+/g, " ");
+                //console.log(postalCode);
+                var hotelData = {
+                  title: str,
+                  url: url,
+                  chiefName: chiefName,
+                  postalCode: postalCodeSTR
+                };
 
-                            var nomRestaurant = li.find('h3').text();
-                            //console.log(nomRestaurant);
-                            var string = nomRestaurant.replace(/\s\s+/g, " ");
-                            //console.log(string);
-                  //          writeStream.write(`${str}; ${string}\n`);
-                              //console.log(string2);
-                            });
-                          }
-                        });
-                      }
-                      catch (e) {console.log('error',e);
-                      }
+                writeStream.write(`${str}; ${chiefName};${postalCodeSTR};${url}\n`);
 
-                      })();
-                  }
-                  else{
-  //                writeStream.write(`${str}; ${string}; ${url}\n`);
-                }
+              }
+            });
+    }
 
 
-          /*      END FORTH REQUEST*/
-                })
-                }
-                  else{
-                    const restos = c('.hotelTabsHeaderTitle');
-                    var nomRestaurant = restos.find('h3').text();
-                    var string2 = nomRestaurant.replace(/\s\s+/g, " ");
 
 
-      //              writeStream.write(`${str};${string2}; ${url}\n`);
-                  }
-                }
-              });
-            }
-            catch (e) {console.log('error',e);
-            }
 
-            })();
-          //END THIRD REQUEST
-
-
-          var hotelWithRestaurant = {
-            title: str,
-            urlhotel: url,
-            urlrestau: urlRestaurant
-          };
-          //console.log(hotelWithRestaurant);
-
-        }
-        }
       });
     }
-    catch (e) {console.log('error',e);
-    }
-
-    })();
-
-      //END SECOND REQUEST
-
-    });
-  }
-});
-}
-catch (e) {console.log('error',e);
-}
-
-})();
+  });
